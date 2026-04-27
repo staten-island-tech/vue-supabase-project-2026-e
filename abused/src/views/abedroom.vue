7e9f7a84472736676bbeb98f441633543c115d80
i-neeed-sleep <kammyl@nycstudents.net>
2026-04-27 14:02:11 -0400

    eeeeee

--- abused/src/views/abedroom.vue
+++ abused/src/views/abedroom.vue
@@ -9,5 +9,39 @@
 </script>
 
 <style scoped>
+.chair{
+    background-image: url();
+    position: absolute;
+    height: 20vw;
+    width: 20vw;
+}
+
+.child{
+    background-image: url();
+    position: absolute;
+    height: 20vw;
+    width: 20vw;
+}
+
+.walking-ltr{
+    animation: walk 1s steps(4) infinite;
+}
+.walking-rtl{
+    transform: scaleX(-1);
+    animation: walk 1s steps(4) infinite;
+}
+@keyframes walk {
+    from {background-position: 0px;}
+    to {background-position: 1500px;}
+}
+@keyframes drawer {
+    from{background-position: 0px;}
+    to {background-image: 1000px;}
+}
+@keyframes shock {
+    from{background-position: 0px;}
+    to{background-position: 1500px;}
+}
+
 
 </style>
