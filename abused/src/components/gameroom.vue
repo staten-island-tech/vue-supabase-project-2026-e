79aabbf9b7843af529eb513a563507c3eb105003
stella785 <stellac35@nycstudents.net>
2026-05-04 14:01:29 -0400

    ...

--- abused/src/components/gameroom.vue
+++ abused/src/components/gameroom.vue
@@ -1,13 +1,30 @@
 <template>
     <div class="container">
         <div class="gameScreen"></div>
-        <div class="story"></div>
+        <div class="story" @mousedown="nextLine"></div>
         <div class="inventory"></div>
     </div>
 </template>
 
 <script>
+import {ref, computed} from 'vue';
 
+const storyLines = [
+    "???: GO TO YOUR ROOM YOU BASTARD",
+    "???: AND STOP BEING A WASTE OF SPACE",
+    "???: GO DO SOMETHING USEFUL IN YOUR LIFE",
+    //insert sound effects (locking child to chair + slamming door + MC opening eyes slowly)
+    "Mc: Ughhhh, so close to escaping that time",
+    "Mc: Great. Now I'm back to square one."
+];
+
+const currentLine = ref(0);
+
+const nextLine = () => {
+    if (currentLine.value < storyLines.length - 1) {
+        currentLine.value++
+    }
+}
 </script>
 
 <style scoped>
