fb1d8fe3e1d544300001b594c164b9194d3fc553
stella785 <stellac35@nycstudents.net>
2026-05-05 13:59:35 -0400

    ...

--- abused/src/views/basement.vue
+++ abused/src/views/basement.vue
@@ -1,15 +1,36 @@
 <template>
     <div>
         <Gameroom/>
+        <div class="story" v-if="basement">{{ nextLinebasement }}</div>
     </div>
 </template>
 
 <script>
 import Gameroom from '@/components/gameroom.vue';
 
+import {ref, computed, onMounted} from 'vue';
 
+const basement = ref(false);
+const basementLine = [
+    "As crusty as ever"
+]
+const nextLinebasement = () => {
+    if (currentLine.value < basementLine.length - 1) {
+        currentLine.value++
+    }
+}
+
+onMounted(() => {
+    basement.value = true;
+})
 </script>
 
 <style scoped>
-
+#child{
+    background-image: url(@/assets/childWalk.png);
+    background-size: cover;
+    position: absolute;
+    height: 320px;
+    width: 200px;
+}
 </style>
--- abused/src/views/garage.vue
+++ abused/src/views/garage.vue
@@ -1,11 +1,35 @@
 <template>
     <div>
-
+        <Gameroom/>
     </div>
+    <div class="toolbox" @click="nextLinetool"></div>
+    <div class="box" @click="nextLinemag"></div>
 </template>
 
 <script>
+import Gameroom from '@/components/gameroom.vue';
+import {ref, computed} from 'vue';
 
+const toolboxLines = [
+    "Who needs car keys when you have this?"
+];
+
+const magazineLines = [
+    "Oops...That's something for older me to look at."
+];
+
+const currentLine = ref(0);
+
+const nextLinetool = () => {
+    if (currentLine.value < toolboxLines.length - 1) {
+        currentLine.value++
+    }
+}
+const nextLinemag = () => {
+    if (currentLine.value < magazineLines.length - 1) {
+        currentLine.value++
+    }
+}
 </script>
 
 <style scoped>
