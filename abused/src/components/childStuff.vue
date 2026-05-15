82799147d70f4488ad4808b3feddff82d407bdf7
i-neeed-sleep <kammyl@nycstudents.net>
2026-05-15 14:01:54 -0400

    eeeeeeeeeeeeeeeeeeee

--- abused/src/components/childStuff.vue
+++ abused/src/components/childStuff.vue
@@ -1,68 +1,74 @@
 <template>
-<div class="map-container" @click="walkTo">
-    <div 
-      id="child" 
-      :class ="action"
-      :style="{ top: y + 'px', left: x + 'px' }"
-    ></div>
-</div>
+    <div id="child" :class="action" :style="{left: charX + 'px', transitionDuration: timeWalk + 's'}"></div>
 </template>
 
 <script>
 
-export default {
-    data() {
-        return {
-            x: 600, 
-            y: -800,
-            action : stand-tr,
-        };
-    },
-    methods: {
-        walkTo(event) {
-            if(this.x > event.clientX){
-                action = walking-ltr
-            }else{
-                action = walking-rtl
-            }
-            this.x = event.clientX;
-            this.y = event.clientY;
-        }
+import { ref } from 'vue';
+
+const action = ref("stand-tr")
+const charX = ref(100)
+const timeWalk = ref(0)
+const distance = ref(0)
+const noAnimation = ref(true)
+
+function walkTo (event) {
+    noAnimation.value = false
+    if(charX.value < (event.clientX-100)){
+        distance.value = event.clientX - charX.value
+        timeWalk.value = distance.value/250
+        action.value = "walking-ltr"
+        setTimeout(()=> {
+            action.value = "stand-tr"
+            noAnimation.value = true
+        }, timeWalk.value*1000)
+    } else{
+        distance.value = charX.value - event.clientX 
+        timeWalk.value = distance.value/250
+        action.value = "walking-rtl"
+        setTimeout(()=> {
+            action.value = "stand-tl"
+            noAnimation.value = true
+        }, timeWalk.value*1000)
     }
-};
+    charX.value = event.clientX - 100
+}
 
 </script>
 
 <style scoped>
 .chair{
     background-image: url(@/assets/childWakeup.png);
+    background-size: cover;
     position: absolute;
-    height: 800px;
-    width: 500px;
+    height: 320px;
+    width: 200px;
 }
 
 #child{
     background-image: url(@/assets/childWalk.png);
     background-size: cover;
     position: absolute;
-    height: 800px;
-    width: 500px;
+    height: 320px;
+    width: 200px;
+    top: 200px;
 }
 
 .stand-tl{
-    transform: scaleX(-1);
     background-position: 0px;
+    animation: look-left 0s steps(2) forwards;;
 }
 .stand-tr{
     background-position: 0px;
 }
 
 .walking-ltr{
-    animation: walk 0.6s steps(4);
+    background-image: url(@/assets/childWalk.png);
+    animation: walk 1s infinite steps(4);
 }
 .walking-rtl{
-    transform: scaleX(-1);
-    animation: walk 0.6s steps(4);
+    background-image: url(@/assets/childWalk.png);
+    animation: walk 1s infinite steps(4), look-left 0s steps(2) forwards;
 }
 
 .open-drawerR{
@@ -72,14 +78,18 @@ export default {
 
 @keyframes walk {
     from {background-position: 0px;}
-    to {background-position: 1500px;}
+    to {background-position: 800px;}
 }
 @keyframes drawer {
     from{background-position: 0px;}
-    to {background-image: 1000px;}
+    to {background-image: 600px;}
 }
 @keyframes shock {
     from{background-position: 0px;}
-    to{background-position: 1500px;}
+    to{background-position: 800px;}
+}
+@keyframes look-left{
+    from{transform: scaleX(1);}
+    to{transform: scaleX(-1);}
 }
 </style>
--- abused/src/views/abedroom.vue
+++ abused/src/views/abedroom.vue
@@ -1,98 +1,26 @@
 <template>
-    <div class="room" @click="walkTo" v-if="noAnimation">    </div>
-        <div id="child" :class="action" :style="{left: charX + 'px', transitionDuration: timeWalk + 's'}"></div>
-
+    <div class="space" @click="walkTo"></div>
+    <ChildStuff ref="child"></ChildStuff>
 </template>
 
 <script setup>
+import ChildStuff from '@/components/childStuff.vue';
 import { ref } from 'vue';
 
-const action = ref("stand-tr")
-const charX = ref(100)
-const timeWalk = ref(0)
-const distance = ref(0)
-const noAnimation = ref(true)
+const child = ref(null)
 
-const walkTo = (event) =>{
-    noAnimation.value = false
-    if(charX.value < (event.clientX-100)){
-        distance.value = event.clientX - charX.value
-        timeWalk.value = distance.value/250
-        action.value = "walking-ltr"
-        setTimeout(()=> {
-            action.value = "stand-tr"
-            noAnimation.value = true
-        }, timeWalk.value*1000)
-    } else{
-        distance.value = charX.value - event.clientX 
-        timeWalk.value = distance.value/250
-        action.value = "walking-rtl"
-        setTimeout(()=> {
-            action.value = "stand-tl"
-            noAnimation.value = true
-        }, timeWalk.value*1000)
-    }
-    charX.value = event.clientX - 100
+function walkTo () {
+    child.value.walkTo;
 }
 
 </script>
 
 <style scoped>
-.room{
+.space{
     width:100vw;
     height: 70vw;
     cursor: crosshair;
     overflow: hidden;
 }
-.chair{
-    background-image: url(@/assets/childWakeup.png);
-    background-size: cover;
-    position: absolute;
-    height: 320px;
-    width: 200px;
-}
 
-#child{
-    background-image: url(@/assets/childWalk.png);
-    background-size: cover;
-    position: absolute;
-    height: 320px;
-    width: 200px;
-    top: 200px;
-}
-
-.stand-tl{
-    background-position: 0px;
-}
-.stand-tr{
-    background-position: 0px;
-}
-
-.walking-ltr{
-    background-image: url(@/assets/childWalk.png);
-    animation: walk 1s infinite steps(4);
-}
-.walking-rtl{
-    background-image: url(@/assets/childWalk.png);
-    transform: scaleX(-1);
-    animation: walk 1s infinite steps(4);
-}
-
-.open-drawerR{
-    background-image: url(@/assets/openDrawer.png);
-    animation: drawer 1s steps(3) ;
-}
-
-@keyframes walk {
-    from {background-position: 0px;}
-    to {background-position: 800px;}
-}
-@keyframes drawer {
-    from{background-position: 0px;}
-    to {background-image: 600px;}
-}
-@keyframes shock {
-    from{background-position: 0px;}
-    to{background-position: 800px;}
-}
 </style>
