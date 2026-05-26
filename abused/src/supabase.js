bca5f17289ee3f3e965be3325362f5367fdd849a
i-neeed-sleep <kammyl@nycstudents.net>
2026-05-26 14:01:28 -0400

    eeeeeeeeeeeeeee

--- abused/src/supabase.js
+++ abused/src/supabase.js
@@ -4,3 +4,9 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
+
+
+const {
+  data: { user },
+} = await supabase.auth.getUser()
+let metadata = user?.user_metadata
