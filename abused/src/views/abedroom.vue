dc9e2296beb903f05d6caf6a474daae31797daf2
i-neeed-sleep <kammyl@nycstudents.net>
2026-06-08 14:01:31 -0400

    eeeeeeeeeeeeee

--- abused/src/views/abedroom.vue
+++ abused/src/views/abedroom.vue
@@ -2,13 +2,15 @@
     <div class="">
         <Gameroom
             :backgroundImage="bedroombg"
+            :basementText="startLines[currentLine]"
             :nextLine="nextLine"
             :inventory="inventory"
         />
         <ChairThing :free="free" @click="chairUnlock"/>
         <ChildWalker v-if="free"/>
         <img :src="bed" class="bed">
-        <img :src="door" class="door">
+        <img :src="door" class="door" @click="lockpick" v-if="!lockpicked">
+        <img :src="door" class="door" @click="unlatch" v-if="lockpicked">
         <img :src="desk" class="desk">
     </div>
     <Transition/>
@@ -23,17 +25,47 @@ import bed from '@/assets/bed.png';
 import door from '@/assets/door.png';
 import { ref } from 'vue';
 import Transition from '@/components/transition.vue';
+import { useRouter } from 'vue-router';
 
 const free = ref(false)
 const inventory = ref([])
+const currentLine = ref(0)
+const startLines = [
+    "Ughhhh, so close to escaping that time", 
+    "Great. Now I'm back to square one."
+]
 
+const lockpicked = ref(false)
 
 
 function chairUnlock(){
     if(inventory.value.includes('bobbyPins')){
-
+        free.value = true
     }else{
+        alert("I need something to unlock this with...")
+    }
+}
 
+function lockpick(){
+    if(inventory.value.includes('bobbyPins')){
+        lockpicked.value = true
+    }else{
+        alert("You're not supposed to be here")
+    }
+}
+
+async function unlatch(){
+    if(inventory.value.includes('ruler')){
+        const { data: { user }, error: autherror } = await supabase.auth.getUser();
+        const { data, error } = await supabase
+            .from('progress')
+            .update({ 'stage': 'basement' })
+            .eq('id', user.id)
+            .select('*')
+        const router = useRouter()
+        router.push(data)
+    }else{
+        alert("I need something to unlock this with...")
     }
 }
 
